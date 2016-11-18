7c4b719305cc46e3fe7f3cc9ea29c4d6a2cd4113
João Gomes <jlmgomes77@gmail.com>
2016-11-18 08:46:57 +0000

    CORS filter.

--- src/server.js
+++ src/server.js
@@ -22,6 +22,12 @@ var server = exports.server = http.createServer(app).listen(port, function () {
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 // Json body parser
 app.use(bodyParser.json()); // support json encoded bodies
